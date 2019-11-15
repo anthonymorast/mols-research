7d161303d41568a15a0f45158c44c074e360423f
Anthony Morast <anthonymorast33@hotmail.com>
2019-11-14 22:04:29 -0700

    finished the serial implementation of the logic to generate all latin squares of a certain order

--- old.ignore/matlab/OriginalCodeMatlab/Old Data/Old SR/gen_norm.m
+++ old.ignore/matlab/OriginalCodeMatlab/Old Data/Old SR/gen_norm.m
@@ -1,10 +1,10 @@
 function gen_norm (B)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % generates all normalized latin squares by permuting
-%    1 2 3 4   then normalizing this matrix to see if 
+%    1 2 3 4   then normalizing this matrix to see if
 %A = 2 1 4 3   it generates any of the 3 other normalized
 %    3 4 1 2   latin squares of order 4.
-%    4 3 2 1   
+%    4 3 2 1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -26,6 +26,7 @@ while ( i < factorial(n) )
     for j=2:n
         for k=2:n
             x = A(j,k);
+            % 11/13/2019 - WTF were you doing? This is literally A(j,k) = sigma(x)
             if x == 1
                 A(j,k) = sigma(1);
             elseif x == 2
@@ -38,6 +39,7 @@ while ( i < factorial(n) )
         end
     end
     %print out latin square
+    display(A);
     fprintf(fid2, '%u %u %u %u ', A);
     fprintf(fid2, '\n');
     i = i+1;
--- src/GenerateSquares.cpp
+++ src/GenerateSquares.cpp
@@ -1,12 +1,32 @@
 #include "LatinSquare.h"
 #include "Utils.h"
-#include "GenerateSquares.h"
 
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
 #include <algorithm>
+
+/// NOTE:
+// We need to pass in checksqs here and only push back the squares if they
+// are not in the allSqs array, that is, if they haven't been checked yet.
+// Otherwise, after we have enough squares all sqs are 'new' and thus added
+// to the checksqs vector. This is a better way to determine which squares
+// haven't actually been checked yet.
+void unique_add_to_vector(LatinSquare sq,
+													vector<LatinSquare> &squares,
+												  vector<LatinSquare> &checkSqs,
+												  bool updateCheckSquares)
+{
+	if(find(squares.begin(), squares.end(), sq) == squares.end())
+	{
+		squares.push_back(sq);
+		if(updateCheckSquares)
+		{
+			checkSqs.push_back(sq);
+		}
+	}
+}
 
 short* get_array_from_line(string line, int size)
 {
@@ -38,11 +58,12 @@ int main(int argc, char* argv[])
 	string filename_n = to_string(order) + "_perm.dat";
 	bool cont = true;
 
-	if(!file_exists(filename_3))
+	// we need this for Main class equivalence (interchanging RCS) not for isotopy class equivalence
+	/*if(!file_exists(filename_3))
 	{
 		cout << filename_3 << " does not exist. Please use the utilites to generate the file." << endl;
 		cont = false;
-	}
+	}*/
    	if (!file_exists(filename_n))
 	{
 		cout << filename_n << " does not exist. Please use the utilites to generate the file." << endl;
@@ -61,52 +82,83 @@ int main(int argc, char* argv[])
 	ofstream sqfile; sqfile.open(to_string(order) + "_squares.dat");
 
 	string line;
-	vector<LatinSquare> allsqs;
-	//while(getline(isofile, line))
+	vector<LatinSquare> allSqs;
+	vector<LatinSquare> checkSqs;		// squares to permute, do not permute all squares everytime
+	while(getline(isofile, line))
 	{
-		getline(isofile, line);
-		ifstream permnfile; permnfile.open(filename_n);
-		short* sqvalues = get_array_from_line(line, order*order);
-		LatinSquare isosq(order, sqvalues);
-		allsqs.push_back(isosq);
-
-//		string symline;
-//		while(getline(permnfile, symline))
-//		{
-//			short *symperm = get_array_from_line(symline, order);
-//			LatinSquare newsq = isosq;
-//			newsq.sym_permutation(symperm);
-//			if(find(allsqs.begin(), allsqs.end(), newsq) == allsqs.end())
-//				allsqs.push_back(newsq);
-//		}
-
-		// a simple hack for adding to vector while iterating
-		int size = allsqs.size();
-		for(int i = 0; i < size; i++)
-		{
-			ifstream perm3file; perm3file.open(filename_3);
-			string rcsline;
-			while(getline(perm3file, rcsline))
-			{
-				LatinSquare sq = allsqs[i];
-				cout << "is symmetric? " << sq.is_symmetric() << "  ;";
-				sq.print_flat();
-				short *rcs = get_array_from_line(rcsline, 3);
-				LatinSquare rcssq = sq;
-				rcssq.rcs_permutation(rcs);
-				if(find(allsqs.begin(), allsqs.end(), rcssq) == allsqs.end())
-				{
-					allsqs.push_back(rcssq);
-					size++;
-				}
-			}
-			perm3file.close();
-		}
-
-		permnfile.close();
+		// get the party started by loading the squares vector with the isotopy
+		// class representatives
+		LatinSquare isoSq(order, get_array_from_line(line, order*order));
+		allSqs.push_back(isoSq);
+		checkSqs.push_back(isoSq);
 	}
 
-	for(auto it = allsqs.begin(); it != allsqs.end(); it++)
+	// keep processing while new squares are added to allSqs
+	int numSqs;
+  do {
+		// set numSqs to current size of the allSqs vector
+		numSqs = allSqs.size();
+		vector<LatinSquare> newSquares;
+		int sqsToCheck = checkSqs.size();
+		int count = 0;
+
+		// for each square to be permuted
+		for(auto it = checkSqs.begin(); it != checkSqs.end(); it++)
+		{
+			if(count > 0 && count % 250 == 0)
+			{
+				cout << "Checking square " << count << " of " << sqsToCheck << endl;
+			}
+
+			ifstream permnfile; permnfile.open(filename_n);
+			string permline;
+			// perform all permutations of row, col, sym
+			while(getline(permnfile, permline))
+			{
+				LatinSquare baseSq = (*it);
+				short* permArr = get_array_from_line(permline, order);
+				LatinSquare rowSq = baseSq;
+				LatinSquare colSq = baseSq;
+				LatinSquare symSq = baseSq;
+
+				rowSq.permute_rows(permArr);
+				colSq.permute_cols(permArr);
+				symSq.permute_symbols(permArr);
+
+				if(!rowSq.is_valid() || !colSq.is_valid() || !symSq.is_valid())
+				{
+						cout << "ERROR!" << endl;
+						cout << "Generated invalid square while applying permutation: " << endl
+								 << endl << permline << " to the square " << endl << endl
+								 << baseSq.tostring() << endl << endl
+								 << "This created the following row, col, and sym squares, "
+								 << "respectively" << endl << endl << rowSq.tostring() << endl << endl
+								 << colSq.tostring() << endl << endl << symSq.tostring() << endl;
+						exit(0);
+				}
+
+				// NOTE: unique only adds saved ~4GB RAM
+				unique_add_to_vector(rowSq, newSquares, checkSqs, false);
+				unique_add_to_vector(colSq, newSquares, checkSqs, false);
+				unique_add_to_vector(symSq, newSquares, checkSqs, false);
+			}
+			count++;
+		}
+
+		// these squares were checked so delete
+		checkSqs.clear();
+		for(auto it = newSquares.begin(); it != newSquares.end(); it++)
+		{
+			unique_add_to_vector((*it), allSqs, checkSqs, true);
+		}
+
+		// process until the number of squares at the end of the while loop
+		// is the same as it was at the start (i.e. until no new squares are added)
+		cout << "Start Count: " << numSqs << ", End Count: " << allSqs.size() << endl;
+	} while (numSqs < allSqs.size());
+
+	// write all squares to a file
+	for(auto it = allSqs.begin(); it != allSqs.end(); it++)
 		sqfile << (*it).flatstring();
 
 	isofile.close();
